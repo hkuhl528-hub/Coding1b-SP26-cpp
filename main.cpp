fe7acc9d6abe6c5e235362cc359eb1eb6f7f0608
Hayden Kuhl <hkuhl528@gmail.com>
2026-03-26 12:04:03 -0500

    built a greeting program

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// Hayden Kuhl Coding 1 spring 2026
+// Our first c++ program
+
+#include <iostream>  // in out stream, for sending text to console.
+#include <string> // for player name
+
+using namespace std;    // so we don't have to type "std::" all the time.
+
+// here is where we start the program
+int main() {
+    // variables declared at the top is best practice
+    string playerName;    // variables use camelCase
+
+    cout << "What is your name, player? ";
+
+    cin >> playerName;
+
+    cout << "Hello " << playerName << "." << endl;
+
+    // cout << "You freaking rock yo" << endl;    // endl =  end line
+
+    return 0;     //end the program
+}
